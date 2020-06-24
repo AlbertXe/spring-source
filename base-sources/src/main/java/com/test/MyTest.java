7735645ad9ef4d7540575ed8dc04b9070465fb7e
AlbertXe <563000409@qq.com>
2020-06-24 16:36:04 +0800

    test

--- base-sources/src/main/java/com/test/MyTest.java
+++ base-sources/src/main/java/com/test/MyTest.java
@@ -1,8 +1,9 @@
-package com;
+package com.test;
 
 import com.po.Student;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
     @Test
@@ -11,5 +12,12 @@ public class MyTest {
         Student student = (Student) context.getBean("student");
         System.out.println(student.getName());
 
+    }
+
+    @Test
+    public void test2() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student.getName());
     }
 }
