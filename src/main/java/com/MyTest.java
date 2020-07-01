c37a9f690005d73f6514e7d9b9f3a2b50d5e35ef
AlbertXe <563000409@qq.com>
2020-07-01 21:08:02 +0800

    spring需要读的源码

--- src/main/java/com/MyTest.java
+++ src/main/java/com/MyTest.java
@@ -3,6 +3,7 @@ package com;
 import com.po.Student;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
     @Test
@@ -10,6 +11,12 @@ public class MyTest {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com");
         Student student = (Student) context.getBean("student");
         System.out.println(student.getName());
+    }
 
+    @Test
+    public void test2() {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        Student student = (Student) context.getBean("student");
+        System.out.println(student.getName());
     }
 }
