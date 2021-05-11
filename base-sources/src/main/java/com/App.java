9c3b687ce685fa0b65e33cb873e0c9b3f1ce09b7
AlbertXe <563000409@qq.com>
2021-05-11 19:56:58 +0800

    tree

---
+++ base-sources/src/main/java/com/App.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * 86150
+ * App
+ * 2021/1/4 20:36
+ */
+@EnableAspectJAutoProxy
+public class App {
+    public static void main(String[] args) {
+    }
+}
---
+++ base-sources/src/main/java/com/test/Atree.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.util.List;
+
+/**
+ * 86150
+ * Atree
+ * 2021/3/31 21:16
+ */
+public class Atree {
+    private String id;
+    private String name;
+    private List<Atree> childes;
+    private Atree parent;
+}
